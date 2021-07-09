e594e4bb926390fe0b5e834c3cac6c21e9ef4742
scxiren <73454440+SereneCai@users.noreply.github.com>
2021-07-09 15:46:47 +0800

    added buttons for future testing

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,25 +1,31 @@
+const caseOne = document.getElementById('caseOne');
+const caseTwo = document.getElementById('caseTwo');
+
+    setInterval(function(){ 
+        const time = document.getElementById('time');
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let day_night = "AM";
+        if(hours > 12){
+            day_night = "PM";
+            hours = hours - 12;
+    
+        }
+        if(minutes < 10){
+            minutes = "0" + minutes;
+        }
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+    
+        time.textContent = hours + ":" + minutes + ":" + seconds + " "+ day_night;
+    
+    }, 10);
 
 
-setInterval(function(){ 
-    const time = document.getElementById('time');
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let day_night = "AM";
-    if(hours > 12){
-        day_night = "PM";
-        hours = hours - 12;
 
-    }
-    if(minutes < 10){
-        minutes = "0" + minutes;
-    }
-    if(seconds < 10){
-        seconds = "0" + seconds;
-    }
 
-    time.textContent = hours + ":" + minutes + ":" + seconds + " "+ day_night;
 
-}, 10);
 
